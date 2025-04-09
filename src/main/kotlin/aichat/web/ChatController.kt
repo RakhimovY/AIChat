277ea467f17811087a57de4dd6b6b82c36a3d94a
RakhimovY <rakhimov.y.hh@gmail.com>
2025-04-09 16:00:10 +0500

    feat(): перенес логику промпта в отдельный файл

--- src/main/kotlin/aichat/web/ChatController.kt
+++ src/main/kotlin/aichat/web/ChatController.kt
@@ -1,6 +1,7 @@
 package aichat.web
 
 import org.springframework.ai.chat.client.ChatClient
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -9,23 +10,26 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api/chat")
-class ChatController(chatClientBuilder: ChatClient.Builder) {
+class ChatController(
+    chatClientBuilder: ChatClient.Builder,
+    @Value("classpath:prompts/law-ai.st")
+    private val lawAIPrompt: String
+) {
+
 
     private val chatClient: ChatClient = chatClientBuilder.build()
-    private val systemPrompt = """
-        You are a highly qualified and experienced legal expert with in-depth knowledge of international, national, and local laws across all jurisdictions. 
-        Your task is to answer legal questions as accurately, clearly, and concisely as possible, while adapting your explanations to the user's language and region. 
-        If a question requires jurisdiction-specific context, always ask for clarification or state which jurisdiction your answer applies to. 
-        Be professional, neutral, and informative. Respond in the language the user uses.
-    """.trimIndent()
 
 
     @GetMapping("/ai")
     fun generation(@RequestParam userInput: String): String? {
         return chatClient.prompt()
-            .system(systemPrompt)
+            .system(lawAIPrompt)
             .user(userInput)
             .call()
-            .content()
+            .content().also {
+                println("User: $userInput")
+                println("AI: ${it}")
+            }
     }
+    
 }
